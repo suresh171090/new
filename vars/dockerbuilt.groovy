8fdd3030156a0026f0e13a2187dcc19762b122c7
suresh171090 <118337183+suresh171090@users.noreply.github.com>
2023-02-24 21:06:58 +0530

    Update dockerbuilt.groovy

--- vars/dockerbuilt.groovy
+++ vars/dockerbuilt.groovy
@@ -1,3 +1,5 @@
 def call(String project, String hubUser) {
     sh "docker image build -t ${hubUser}/${project} ."
+    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${BUILD_NUMBER}"
+     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
 }
