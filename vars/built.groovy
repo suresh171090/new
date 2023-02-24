3d3b364c45cbbacda744c214f01577317c95fc63
suresh171090 <118337183+suresh171090@users.noreply.github.com>
2023-02-24 21:05:32 +0530

    Update built.groovy

--- vars/built.groovy
+++ vars/built.groovy
@@ -2,4 +2,6 @@ def call(){
 def mvnHome =  tool name: 'maven3', type: 'maven'   
       sh "${mvnHome}/bin/mvn clean package"
 	  sh 'mv target/myweb*.war target/newapp.war'
+	sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${BUILD_NUMBER}"
+        sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
 }
