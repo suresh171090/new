5ef7d8b4423fc11951d116997a06fcc484c1f16d
suresh171090 <118337183+suresh171090@users.noreply.github.com>
2023-02-24 22:00:40 +0530

    Create dockerrun.groovy

---
+++ vars/dockerrun.groovy
@@ -0,0 +1,9 @@
+def call(String project, String hubUser, String contname) {
+	try{
+		sh 'docker rm -f tomcattest'
+	}catch(error){
+		//  do nothing if there is an exception
+	}
+  sh "docker run -d -p 8090:8080 --name ${contname} ${hubUser}/${project}:latest"
+}
+  
